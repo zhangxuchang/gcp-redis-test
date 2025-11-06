e7bb6cfe1f0de74b34f314c04e8c6e250f01a852
zhangxuxuchang <44724385@qq.com>
2025-11-06 13:23:49 +0800

    *

--- gcp/src/main/kotlin/BatchValkeyClusterTest.kt
+++ gcp/src/main/kotlin/BatchValkeyClusterTest.kt
@@ -44,7 +44,7 @@ object BatchValkeyClusterTest {
         //  实际使用时请连接到集群的多个节点
         val jedis = JedisCluster(jedisClusterNodes)
         runBlocking {
-            highConcurrencyBatchWrite(jedis, 2, 10)
+            highConcurrencyBatchWrite(jedis, 20, 1000)
         }
     }
 }
