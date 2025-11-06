266f37a8003eb3221d779b7df9be1f653d9a830c
zhangxuxuchang <44724385@qq.com>
2025-11-06 13:25:57 +0800

    *

--- gcp/src/main/kotlin/BatchClusterTest.kt
+++ gcp/src/main/kotlin/BatchClusterTest.kt
@@ -45,7 +45,7 @@ object BatchClusterTest {
         //  实际使用时请连接到集群的多个节点
         val jedis = JedisCluster(jedisClusterNodes)
         runBlocking {
-            highConcurrencyBatchWrite(jedis, 20, 1000)
+            highConcurrencyBatchWrite(jedis, 30, 2000)
         }
     }
 }
